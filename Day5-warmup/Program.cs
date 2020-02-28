d591ffbdec7a450ab840afde82329f81b49056fc
Diana Lim <dianalim@Dianas-MacBook-Pro.local>
2020-02-28 09:10:03 -0600

    starting template for revature's first C# warmup task.

---
+++ Day5-warmup/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Warmup1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please type in a multi-term word.");
+        }
+    }
+}
