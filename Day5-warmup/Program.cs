8334e0b58a4ffaedd325b1623ab7c91d207fa147
Diana Lim <dianalim@Dianas-MacBook-Pro.local>
2020-02-28 09:23:39 -0600

    Completed solution for first C# warmup task.

--- Day5-warmup/Program.cs
+++ Day5-warmup/Program.cs
@@ -7,6 +7,12 @@ namespace Warmup1
         static void Main(string[] args)
         {
             Console.WriteLine("Please type in a multi-term word.");
+            string contain = Console.ReadLine();
+
+            string[] containSplit = contain.Split(" ");
+            foreach (string res in containSplit) {
+            Console.Write(res.Substring(0,1).ToUpper());
+            }
         }
     }
 }
