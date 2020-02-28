876cd90c507fd8038c7bc5f70026708f8c8ea022
Diana Lim <dianalim@Dianas-MacBook-Pro.local>
2020-02-28 09:32:31 -0600

    I added comments to show my thinking process for future reference.

--- Day5-warmup/Program.cs
+++ Day5-warmup/Program.cs
@@ -4,12 +4,18 @@ namespace Warmup1
 {
     class Program
     {
+        //Task description: Create a program that asks for a multi-term word and prints the acronym for each word.
         static void Main(string[] args)
         {
+            // My Process:
+            // 1. Ask for a multi-term word and store it in a variable.
             Console.WriteLine("Please type in a multi-term word.");
             string contain = Console.ReadLine();
 
+            // 2. Split each word and store it into an array.
             string[] containSplit = contain.Split(" ");
+            
+            // 3. Loop through each word and print the first letter of each in uppercase.
             foreach (string res in containSplit) {
             Console.Write(res.Substring(0,1).ToUpper());
             }
