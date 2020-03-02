bae53bc6b7955e3fbcf8495645777eaddde42285
Diana Lim <dianalim@Dianas-MacBook-Pro.local>
2020-03-02 14:14:10 -0600

    I added the task description for future reference.

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -4,6 +4,15 @@ namespace FizzBuzz
 {
     class Program
     {
+        /*Task Description:
+            For each number from 1 to 1000 in order,
+            * print "Fizz" for the ones divisible by 3 (& not 5)
+            * print "Buzz" for the ones divisible by 5 (& not 3)
+            * print "Fizzbuzz" for the ones divisible by both 3 and 5
+            * print the number itself, for all the rest of the numbers
+            Also, I want to know, at the end, how many Fizz, how many Buzz, how many Fizzbuzz.
+            Your code should calculate that, and, put the numbers you get in a comment, so I
+            can see them without running the code. */
         static void Main(string[] args)
         {
             for (int i = 1; i <= 1000; i++)
