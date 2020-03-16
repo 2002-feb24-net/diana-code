b68c327b2a6fb250f3e37159beb87eff052737a8
Diana Lim <dianalim@Dianas-MacBook-Pro.local>
2020-03-16 13:21:25 -0500

    I tried something else.

--- Palindrome/palindrome/Class1.cs
+++ Palindrome/palindrome/Class1.cs
@@ -4,6 +4,20 @@ namespace palindrome
 {
     public class Class1: IPalindrome
     {
+            public static string Palindrome(string word)
+        {
+            // my previous regex /[^0-9a-zA-Z]/g,''
+            string word = word.Replace(@"^\w+$","").ToLower();
+            string wordForward = word;
+            string wordBackward = word.Split("").Reverse().Join("");
+            if(wordForward == wordBackward)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        } 
     }
 
     interface IPalindrome
