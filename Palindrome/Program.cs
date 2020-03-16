5fb47230e05bc81d96eabe3e804451011f3f3445
Diana Lim <dianalim@Dianas-MacBook-Pro.local>
2020-03-16 12:22:31 -0500

    Current attempt at challenge

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -8,5 +8,19 @@ namespace Palindrome
         {
             Console.WriteLine("Hello World!");
         }
+
+        public static string Palindrome(string word)
+        {
+            string word = word.Replace(/[^0-9a-zA-Z]/g,'').ToLower();
+            string wordForward = word;
+            string wordBackward = word.Split("").Reverse().Join("");
+            if(wordForward == wordBackward)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        } 
     }
 }
---
+++ Palindrome/testclass.cs
@@ -0,0 +1,11 @@
+using Xunit;
+
+public class testclass
+{
+    [Fact]
+    public void PassingPalindromeTest()
+    {
+        Assert.Equal(kayak, Program.Palindrome(kayak));
+
+    }
+}
